1084dbf582b2d46c63e69245dde3041b33b20ccc
christmas <Teerachat20005@gmail.com>
2026-06-22 15:06:13 +0700

    Update database.js

--- backend-api/src/config/database.js
+++ backend-api/src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: "210.246.215.19",
-  port: 3306,
-  user: "thief_user",
-  password: "ThiefDetect@2026_Secure!",
-  database: "thief_detection"
+  host: "",
+  port: ,
+  user: "",
+  password: !",
+  database: ""
 };
